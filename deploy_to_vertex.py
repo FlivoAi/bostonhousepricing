b366c50b9035e38c96b676ab43340a38d372cde6
FlivoAi <admin@flivo.ai>
2024-06-28 18:52:23 +0530

    Update deploy_to_vertex.py

--- deploy_to_vertex.py
+++ deploy_to_vertex.py
@@ -4,25 +4,17 @@ from google.cloud import aiplatform
 def deploy_model():
     from google.cloud import aiplatform
     project_id = os.getenv('GOOGLE_PROJECT')
-    bucket= os.getenv('GCS_BUCKET_NAME')
+    # bucket= os.getenv('GCS_BUCKET_NAME')
     region = "us-central1"  
     # Initialize the Vertex AI client
     aiplatform.init(project=os.getenv(project_id), location=region)
         # Upload the model
     model = aiplatform.Model.upload(
         display_name='my-model',
-        # url_model="regmodel.pkl"
         artifact_uri='gs://flivo-demo',
         serving_container_image_uri=f'us-central1-docker.pkg.dev/{project_id}/test1/demo-test-1',
     )
-    #creating endpoint 
-    endpoint = aiplatform.Endpoint.create(
-    project=project_id,
-    display_name="ml-endpoint",
-    location=region,  # Optional, change if needed
-    )
-    print(f"Endpoint created: {endpoint.name}")
-
+    model.wait()
     # Deploy the model to an endpoint
     endpoint = model.deploy(
         deployed_model_display_name='ml-endpoint',
