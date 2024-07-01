17e6b6db1500e39cecfcefd98591a4a122ac5f83
FlivoAi <admin@flivo.ai>
2024-07-01 17:00:43 +0530

    Update deploy_to_vertex.py

--- deploy_to_vertex.py
+++ deploy_to_vertex.py
@@ -18,7 +18,7 @@ def deploy_model():
     # Deploy the model to an endpoint
     endpoint = model.deploy(
         deployed_model_display_name='ml-endpoint',
-        machine_type='n2-standard-8',
+        machine_type='c2-standard-60',
         min_replica_count=2,
         max_replica_count=4,
     )
